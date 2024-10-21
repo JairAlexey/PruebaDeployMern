7086ad23161da88c98527dc799159a62f1872573
JairAlexey <alexeyrueda823@gmail.com>
2024-10-21 14:09:08 -0500

    agregando mongouri a config

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI =
+export const MONGODB_URI = "mongodb+srv://alexeyrueda1122:alexeyrueda1122@crudproductos.iexjo.mongodb.net/?retryWrites=true&w=majority&appName=CrudProductos";
   process.env.MONGODB_URI || "mongodb://localhost/mern-tasks";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
