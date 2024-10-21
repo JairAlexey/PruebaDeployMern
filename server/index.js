d1a88dd7140fbf1c316d1dc7a32b791a6b9258e7
JairAlexey <alexeyrueda823@gmail.com>
2024-10-21 14:32:31 -0500

    pruebas produccio

--- server/index.js
+++ server/index.js
@@ -2,9 +2,6 @@ import app from "./src/app.js";
 import { PORT } from "./src/config.js";
 import { connectDB } from "./src/db.js";
 
-import dotenv from 'dotenv';
-dotenv.config();
-
 async function main() {
   try {
     await connectDB();
--- server/src/config.js
+++ server/src/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 3000;
 export const MONGODB_URI = "mongodb+srv://alexeyrueda1122:alexeyrueda1122@crudproductos.iexjo.mongodb.net/?retryWrites=true&w=majority&appName=CrudProductos";
   process.env.MONGODB_URI || "mongodb://localhost/mern-tasks";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
